c76cbca10485cf93375b9fdbbedd2065021685cd
Julian <rojul@users.noreply.github.com>
2017-12-12 15:33:51 +0100

    Move id validation to utils

--- api/quizzes.js
+++ api/quizzes.js
@@ -3,10 +3,9 @@ const fs = require('fs-extra')
 const path = require('path')
 
 const config = require('../config')
+const utils = require('../utils')
 
 const router = express.Router()
-
-const validateQuizId = id => /^[a-z0-9]+$/.test(id)
 
 const readJson = async id => {
   const json = await fs.readJson(path.join(config.configPath, `${id}.json`))
@@ -18,7 +17,7 @@ router.get('/', async (req, res, next) => {
   try {
     const ids = (await fs.readdir(config.configPath))
       .map(f => f.split('.'))
-      .filter(f => f.length === 2 && f[1] === 'json' && validateQuizId(f[0]))
+      .filter(f => f.length === 2 && f[1] === 'json' && utils.validateId(f[0]))
       .map(f => f[0])
     const promises = ids.map(id => readJson(id))
     const quizzes = (await Promise.all(promises))
@@ -34,7 +33,7 @@ router.get('/', async (req, res, next) => {
 
 router.get('/:id', async (req, res, next) => {
   const id = req.params.id
-  if (!validateQuizId(id)) {
+  if (!utils.validateId(id)) {
     res.status(400).json({ error: 'Invalid ID' })
     return
   }
