521ebcf80378e6f48935b99ebe94e7066068a732
Julian <rojul@users.noreply.github.com>
2017-12-13 09:49:45 +0100

    400 and 500 error handling

--- api/api.js
+++ api/api.js
@@ -3,6 +3,7 @@ const express = require('express')
 const { version, description } = require('../package.json')
 const query = require('./query')
 const quizzes = require('./quizzes')
+const logger = require('../logger')
 
 const router = express.Router()
 
@@ -11,6 +12,19 @@ router.use('/quizzes', quizzes)
 
 router.get('/', (req, res, next) => {
   res.json({ version, description })
+})
+
+router.all('*', (req, res) => {
+  res.status(404).json({ error: 'Resource Not Found' })
+})
+
+router.use((err, req, res, next) => {
+  logger.error(err)
+  if (res.headersSent) {
+    next(err)
+    return
+  }
+  res.status(500).json({ error: err.sqlMessage || err.message })
 })
 
 module.exports = router
