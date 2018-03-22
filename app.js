a1825c2b2d6e21deb3479a9cf55adff5efd7d623
Julian <rojul@users.noreply.github.com>
2018-03-22 13:13:36 +0100

    Always send index.html

--- app.js
+++ app.js
@@ -10,4 +10,8 @@ app.use(compression())
 app.use('/api', api)
 app.use(express.static('dist'))
 
+app.get('*', (req, res) => {
+  res.sendFile(__dirname + '/dist/index.html')
+})
+
 module.exports = app
